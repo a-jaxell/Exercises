b42c078a8143cae741eecec455d72f3afdcf1b66
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-09-16 13:43:00 +0200

    Refactor and rename to show intent more clearly

--- src/test/java/org/Labb3/service/WarehouseTest.java
+++ src/test/java/org/Labb3/service/WarehouseTest.java
@@ -3,6 +3,7 @@ package org.Labb3.service;
 import org.Labb3.FakeProducts;
 import org.Labb3.entities.Product;
 import org.Labb3.entities.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Clock;
@@ -18,33 +19,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTest {
 
+    private Warehouse warehouse;
+    private Product product;
+    private LocalDateTime fixedDate;
+    private LocalDateTime fixedDate2;
+    private LocalDateTime fixedDate3;
 
-    Clock clock = Clock.fixed(Instant.ofEpochSecond(1631510400, 1000L), ZoneId.of("UTC"));
-    Clock clock2 = Clock.fixed(Instant.ofEpochSecond(1633504400, 1000L), ZoneId.of("UTC"));
-    Clock clock3 = Clock.fixed(Instant.ofEpochSecond(1637524400, 1000L), ZoneId.of("UTC"));
-    LocalDateTime fixedDate = LocalDateTime.now(clock);
-    LocalDateTime fixedDate2 = LocalDateTime.now(clock2);
-    LocalDateTime fixedDate3 = LocalDateTime.now(clock3);
-    Warehouse warehouse = new Warehouse();
-    Product product = new Product("Microplane", ProductCategory.UTENSILS, 5);
+    @BeforeEach
+            void setUp(){
+            Clock clock = Clock.fixed(Instant.ofEpochSecond(1631510400, 1000L), ZoneId.of("UTC"));
+            Clock clock2 = Clock.fixed(Instant.ofEpochSecond(1633504400, 1000L), ZoneId.of("UTC"));
+            Clock clock3 = Clock.fixed(Instant.ofEpochSecond(1637524400, 1000L), ZoneId.of("UTC"));
 
+            fixedDate = LocalDateTime.now(clock);
+            fixedDate2 = LocalDateTime.now(clock2);
+            fixedDate3 = LocalDateTime.now(clock3);
+
+            warehouse = new Warehouse();
+            product = new Product("Microplane", ProductCategory.UTENSILS, 5);
+    }
+    private Product createProduct(String name, ProductCategory category, int rating){
+        return new Product(name, category, rating);
+    }
+    private Product createProduct(String name, ProductCategory category, int rating, LocalDateTime date){
+        return new Product(name, category, rating, date);
+    }
 
     @Test
-    void testAddNewProduct() {
+    void shouldAddNewProductToWarehouse() {
         assertThat(warehouse.getProducts()).isNullOrEmpty();
         warehouse.addNewProduct(product);
         assertThat(warehouse.getProducts()).isNotEmpty();
     }
 
     @Test
-    void testGetAllProducts() {
-        List<Product> productList = warehouse.getProducts();
+    void shouldGetAllProducts() {
         warehouse.addNewProduct(product);
-        assertThat(productList).isExactlyInstanceOf(warehouse.getProducts().getClass());
+        List<Product> productList = warehouse.getProducts();
+        assertEquals(productList.size(), 1, "Warehouse should contain one product");
+        assertEquals(productList.get(0), product, "Warehouse should contain the added product");
     }
 
     @Test
-    void testGettingProductWithIdShouldReturnProduct() {
+    void testGettingProductWithId_ShouldReturnProduct() {
         warehouse.addNewProduct(product);
         UUID id = product.getId();
         Product actual = warehouse.getProduct(id);
@@ -52,7 +69,7 @@ class WarehouseTest {
     }
 
     @Test
-    void testGettingProductWithWrongIdShouldThrowException() {
+    void testGettingProductWithWrongId_ShouldThrowException() {
         UUID id = product.getId();
         UUID wrongId = UUID.randomUUID();
         assertEquals(id.getClass(), wrongId.getClass());
@@ -61,8 +78,7 @@ class WarehouseTest {
     }
 
     @Test
-    void testModifyingProductSucceeds() throws InterruptedException {
-        Warehouse warehouse = new Warehouse();
+    void testModifyingProduct_ShouldSucceed() throws InterruptedException {
         //Calling alternate constructor for ability to set a fixed date.
         Product product = new Product("Cheese_Grater", ProductCategory.UTENSILS, 5, fixedDate);
         warehouse.addNewProduct(product);
@@ -83,7 +99,7 @@ class WarehouseTest {
     }
 
     @Test
-    void testSortingProductsByNamesAlphabetically() {
+    void testSortingProductsAlphabetically_ShouldSucceed() {
 
         Warehouse warehouse = new Warehouse(FakeProducts.list());
         List<Product> expected = warehouse.sortByName();
@@ -93,12 +109,12 @@ class WarehouseTest {
                 .toList();
         List<Product> notActual = FakeProducts.list();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "Products should sort alphabetically(a-z)");
         assertNotEquals(expected, notActual);
     }
 
     @Test
-    void testGettingAllProductsCreatedAfterACertainDate() {
+    void shouldReturnAllProductsCreatedAfterACertainDate() {
         Warehouse warehouse1 = new Warehouse();
         warehouse1.addNewProduct(new Product("Sauce_Stirrer", ProductCategory.WHISKS, 1, fixedDate3));
         warehouse1.addNewProduct(new Product("Cheese_Grater", ProductCategory.UTENSILS, 5, fixedDate2));
@@ -110,7 +126,7 @@ class WarehouseTest {
     }
 
     @Test
-    void testGettingAllModifiedAfterCreated() {
+    void shouldReturnAllModifiedProducts() {
         Warehouse warehouse1 = new Warehouse();
 
         warehouse1.addNewProduct(new Product("Sauce_Stirrer", ProductCategory.WHISKS, 1, fixedDate3));
