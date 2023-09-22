d9aa2da2c7195df5d309ab49035b0bbd72a2ac38
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-09-22 23:03:46 +0200

    Add GeneralDiscount with interfaces

---
+++ src/main/java/org/sysarkLabb1/GeneralDiscount/GeneralDiscount.java
@@ -0,0 +1,31 @@
+package org.sysarkLabb1.GeneralDiscount;
+
+import org.sysarkLabb1.Discounts.Discount;
+import org.sysarkLabb1.entities.Product;
+
+public class GeneralDiscount implements Discount{
+
+    private final Discount nextDiscount;
+    private final IsApplicable isApplicable;
+    private final CalculateDiscount calculateDiscount;
+    private final String description;
+
+    public GeneralDiscount(Discount discount, IsApplicable isApplicable, CalculateDiscount calculateDiscount, String description){
+        this.nextDiscount = discount;
+        this.calculateDiscount = calculateDiscount;
+        this.isApplicable = isApplicable;
+        this.description = description;
+    }
+    public double apply(Product product) {
+        return calculateDiscount(product);
+    }
+
+    public String getDescription(Product product) {
+        String desc = (isApplicable.isApplicable(product) ? description : "");
+        return nextDiscount.getDescription(product) + " " + desc;
+    }
+    public double calculateDiscount(Product product) {
+        return isApplicable.isApplicable(product) ? (nextDiscount.apply(product) + calculateDiscount.calculateDiscount(product)) : (nextDiscount.apply(product) + 0);
+    }
+
+}
--- src/main/java/org/sysarkLabb1/PurchaseTest.java
+++ src/main/java/org/sysarkLabb1/PurchaseTest.java
@@ -10,9 +10,15 @@ public class PurchaseTest {
         Product concreteBlock = new Product("Concrete Block", 35.90, 20);
         Product decorateMagazine = new Product("Decor Monthly", 52.75, 5);
         Product comboDiscount = new Product("Milky milk milk", 7.0, 10);
+        Product generalDiscount = new Product("Finest dirt", 1000, 1);
 
         Discount discount = new QuantityDiscount(new FridayDiscount(new MilkDiscount(new EmptyDiscount())));
+        Discount discount1 = new FridayDiscount(new GeneralDiscount(new EmptyDiscount(), (product) -> product.price() > 100, product -> product.price()* 0.80,"20% off on items above 100kr"));
 
+        System.out.println(discount1.apply(generalDiscount));
+        System.out.println(discount1.getDescription(generalDiscount));
+
+        /*
         System.out.println(discount.apply(wholeMilk));
         System.out.println(discount.getDescription(wholeMilk));
         System.out.println(discount.apply(concreteBlock));
@@ -21,5 +27,6 @@ public class PurchaseTest {
         System.out.println(discount.getDescription(decorateMagazine));
         System.out.println(discount.apply(comboDiscount));
         System.out.println(discount.getDescription(comboDiscount));
+         */
     }
 }
